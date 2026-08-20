654b3f63b055816b57f4d91a7209c0e79a8489af
Hannita17 <hannalopezluna@gmail.com>
2026-08-20 14:46:13 -0600

    Configurar rutas de login home y perfil

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Perfil } from './perfil/perfil';
+import { authGuard } from './auth-guard';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'home',
+    component: Home,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'perfil',
+    component: Perfil,
+    canActivate: [authGuard]
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,41 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet],
+  templateUrl: './app.html'
 })
-export class App {
-  estadoBackend = '⚪ Sin verificar';
-  estadoBaseDatos = '⚪ Sin verificar';
-
-  async verificarConexion() {
-    try {
-      const health = await fetch(
-        'https://projectgym-t958.onrender.com/health'
-      );
-
-      if (health.ok) {
-        this.estadoBackend = '🟢 Conectado';
-      } else {
-        this.estadoBackend = '🔴 Error';
-      }
-
-      const ping = await fetch(
-        'https://projectgym-t958.onrender.com/ping'
-      );
-
-      if (ping.ok) {
-        this.estadoBaseDatos = '🟢 Conectada';
-      } else {
-        this.estadoBaseDatos = '🔴 Error';
-      }
-
-    } catch {
-      this.estadoBackend = '🔴 Sin conexión';
-      this.estadoBaseDatos = '🔴 Sin conexión';
-    }
-  }
-}
+export class App {}
