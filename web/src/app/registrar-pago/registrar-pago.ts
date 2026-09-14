11bf0b072ab4954bc6349bff44ded4e100d87276
Hannita17 <hannalopezluna@gmail.com>
2026-09-14 14:43:17 -0600

    Agregar campos del formulario de pago

--- web/src/app/registrar-pago/registrar-pago.ts
+++ web/src/app/registrar-pago/registrar-pago.ts
@@ -3,17 +3,32 @@ import {
 } from '@angular/core';
 
 import {
+  CommonModule
+} from '@angular/common';
+
+import {
+  FormsModule
+} from '@angular/forms';
+
+import {
   Router
 } from '@angular/router';
 
 @Component({
   selector: 'app-registrar-pago',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './registrar-pago.html',
   styleUrl: './registrar-pago.css'
 })
 export class RegistrarPago {
+
+  monto: number | null = null;
+
+  tipoPago = '';
 
   constructor(
     private router: Router
